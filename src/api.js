f92af384cbb1a59c9de3d718710ef76050b00428
onareshh <naresh492003@gmail.com>
2024-09-08 13:32:41 +0530

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+// Add this line to define the WEATHER_API_URL
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "f45d252a4b660207cbf626885a2339e0"; // enter your key from openweather API
